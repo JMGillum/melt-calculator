abb882bdebf15412f14d2b9d955ff03a8a90f755
JMGillum <JMGillum@protonmail.com>
2025-07-18 20:30:28 -0700

    Added missing data from v0.2.2

--- coinInfo.py
+++ coinInfo.py
@@ -3,6 +3,7 @@ from tree.tree import Tree
 from tree.node import Node
 from metals import Metals
 from purchases import purchases
+import weights
 
 class NamedList:
     def __init__(self, name, items):
@@ -33,6 +34,18 @@ class NamedList:
 
 class Coins:
     coins = {
+        # Canada
+        "canada_dollar_5": Node(CoinData(
+            nickname="Silver Maple Leaf Bullion",
+            years=list(range(1988,2026)),
+            weight=31.11,
+            fineness=0.9999,
+            precious_metal_weight=weights.Weight(1,weights.Units.TROY_OUNCES),
+            country="Canada",
+            face_value=5,
+            denomination="Dollars"
+        )),
+        # France
         "centimes_20": Node(data= CoinData(
             years=list(range(1848, 1921)),
             weight=1,
@@ -186,10 +199,167 @@ class Coins:
             face_value=100,
             denomination="Franc"
         )),
+        # Germany
+        "mark_10": Node(CoinData(
+            years=list(range(1871,1916)),
+            weight=3.9825,
+            fineness=0.900,
+            retention=0.97,
+            metal=Metals.GOLD,
+            country="Germany",
+            face_value=10,
+            denomination="Mark"
+        )),
+        # Italy
+        "centesimi_20": Node(CoinData(
+            years=list(range(1863,1918)),
+            weight=1,
+            fineness=0.835,
+            metal=Metals.SILVER,
+            country="Italy",
+            face_value=20,
+            denomination="Centesimi"
+        )),
+        # Mexico
+        "peso_1": Node(CoinData(
+            years=[x for x in list(range(1920,1946)) if x not in list(range(1928,1932))+[1936,1937,1939,1941,1942]],
+            weight=16.66,
+            fineness=0.72,
+            metal=Metals.SILVER,
+            country="Mexico",
+            face_value=1,
+            denomination="Peso"
+        )),
+        # United States
+        "barber_dime": Node(CoinData(
+        nickname="Barber Dime",
+        years=[x for x in list(range(1892, 1917))],
+        weight=2.5,
+        fineness=0.900,
+        metal=Metals.SILVER,
+            country="United States",
+            face_value=10,
+            denomination="Cents"
+        )),
+        "mercury_dime": Node(CoinData(
+        nickname="Mercury Dime",
+        years=[x for x in list(range(1916, 1946)) if x not in [1922, 1932, 1933]],
+        weight=2.5,
+        fineness=0.900,
+        metal=Metals.SILVER,
+            country="United States",
+            face_value=10,
+            denomination="Cents"
+        )),
+        "roosevelt_dime": Node(CoinData(
+        nickname="Roosevelt Dime",
+        years=[x for x in list(range(1946, 1965))],
+        weight=2.5,
+        fineness=0.900,
+        metal=Metals.SILVER,
+            country="United States",
+            face_value=10,
+            denomination="Cents"
+        )),
+        "barber_quarter": Node(CoinData(
+        nickname="Barber Quarter",
+        years=list(range(1892,1917)),
+        weight=6.25,
+        fineness=0.900,
+        metal=Metals.SILVER,
+            country="United States",
+            face_value=25,
+            denomination="Cents"
+        )),
+        "standing_liberty_quarter": Node(CoinData(
+        nickname="Standing Liberty Quarter",
+        years=[x for x in list(range(1916, 1931)) if x not in [1922]],
+        weight=6.25,
+        fineness=0.900,
+        metal=Metals.SILVER,
+            country="United States",
+            face_value=25,
+            denomination="Cents"
+        )),
+        "washington_quarter": Node(CoinData(
+        nickname="Washington Quarter",
+        years=[x for x in list(range(1932, 1965)) if x not in [1933]],
+        weight=6.25,
+        fineness=0.900,
+        metal=Metals.SILVER,
+            country="United States",
+            face_value=25,
+            denomination="Cents"
+        )),
+        "walking_liberty_half": Node(CoinData(
+        nickname="Walking Liberty Half",
+        years=[
+            x
+            for x in list(range(1916, 1948))
+            if x not in ([1922] + list(range(1924, 1927)) + list(range(1930, 1933)))
+        ],
+        weight=12.5,
+        fineness=0.900,
+        metal=Metals.SILVER,
+        country="United States",
+            face_value=50,
+            denomination="Cents"
+        )),
+        "benjamin_half": Node(CoinData(
+        nickname="Benjamin Half",
+        years=[x for x in list(range(1948, 1964))],
+        weight=12.5,
+        fineness=0.900,
+        metal=Metals.SILVER,
+        country="United States",
+            face_value=50,
+            denomination="Cents"
+        )),
+        "kennedy_half_1": Node(CoinData(
+        nickname="90% Kennedy Half",
+        years=[1964],
+        weight=12.5,
+        fineness=0.900,
+        metal=Metals.SILVER,
+        country="United States",
+            face_value=50,
+            denomination="Cents"
+        )),
+        "kennedy_half_2": Node(CoinData(
+        nickname="40% Kennedy Half",
+        years=[x for x in list(range(1965, 1971))],
+        weight=11.5,
+        fineness=0.400,
+        metal=Metals.SILVER,
+        country="United States",
+            face_value=50,
+            denomination="Cents"
+        )),
+        "morgan_dollar": Node(CoinData(
+        nickname="Morgan Dollar",
+        years=[x for x in (list(range(1878, 1905)) + [1921])],
+        weight=26.73,
+        fineness=0.900,
+        metal=Metals.SILVER,
+        country="United States",
+            face_value=1,
+            denomination="Dollar"
+        )),
+        "peace_dollar": Node(CoinData(
+        nickname="Peace Dollar",
+        years=[x for x in (list(range(1921, 1929)) + [1934, 1935])],
+        weight=26.73,
+        fineness=0.900,
+        metal=Metals.SILVER,
+        country="United States",
+            face_value=1,
+            denomination="Dollar"
+        )),
     }
 
     # Used to build the tree from just a coin object
     coins_reverse_build = {
+        "canada_dollar_5": ("canada_dollar_5","canada_dollar","canada"),
         "centimes_20": ("centimes_20","centimes","france"),
         "centimes_50_1": ("centimes_50","centimes","france"),
         "centimes_50_2": ("centimes_50","centimes","france"),
@@ -207,11 +377,29 @@ class Coins:
         "franc_100_1": ("franc_100","franc","france"),
         "franc_100_2": ("franc_100","franc","france"),
         "franc_100_3": ("franc_100","franc","france"),
+        "mark_10": ("mark_10","mark","germany"),
+        "centesimi_20": ("centesimi_20","centesimi","italy"),
+        "peso_1": ("peso_1","peso","mexico"),
+        "barber_dime": ("dime","cents","united_states"),
+        "mercury_dime": ("dime","cents","united_states"),
+        "roosevelt_dime": ("dime","cents","united_states"),
+        "barber_quarter": ("quarter","cents","united_states"),
+        "standing_liberty_quarter": ("quarter","cents","united_states"),
+        "washington_quarter": ("quarter","cents","united_states"),
+        "walking_liberty_half": ("half","cents","united_states"),
+        "benjamin_half": ("half","cents","united_states"),
+        "kennedy_half_1": ("half","cents","united_states"),
+        "kennedy_half_2": ("half","cents","united_states"),
+        "morgan_dollar": ("dollar","dollar","united_states"),
+        "peace_dollar": ("dollar","dollar","united_states"),
     }
 
 
     # Indicates which coins are made of silver
     silver_coins = [
+        # Canada
+        "canada_dollar_5",
+        # France
         "centimes_20",
         "centimes_50_1",
         "centimes_50_2",
@@ -223,19 +411,42 @@ class Coins:
         "franc_10_1",
         "franc_20_2",
         "franc_100_1",
+        # Italy
+        "centesimi_20",
+        # Mexico
+        "peso_1",
+        # United States
+        "barber_dime",
+        "mercury_dime",
+        "roosevelt_dime",
+        "barber_quarter",
+        "standing_liberty_quarter",
+        "washington_quarter",
+        "walking_liberty_half",
+        "benjamin_half",
+        "kennedy_half_1",
+        "kennedy_half_2",
+        "morgan_dollar",
+        "peace_dollar",
     ]
 
     # Indicates which coins are made of gold
     gold_coins = [
+        # France
         "franc_5_3",
         "franc_10_2",
         "franc_20_1",
         "franc_50",
         "franc_100_2",
         "franc_100_3",
+        # Germany
+        "mark_10",
     ]
 
     values = {
+        # Canada
+        "canada_dollar_5": NamedList("5",["canada_dollar_5"]),
+        # France
         "centimes_20": NamedList("20", ["centimes_20"]),
         "centimes_50": NamedList("50", ["centimes_50_1", "centimes_50_2"]),
         "franc_1": NamedList("1", ["franc_1_1", "franc_1_2"]),
@@ -245,9 +456,23 @@ class Coins:
         "franc_20": NamedList("20", ["franc_20_1", "franc_20_2"]),
         "franc_50": NamedList("50", ["franc_50"]),
         "franc_100": NamedList("100", ["franc_100_1", "franc_100_2", "franc_100_3"]),
+        # Germany
+        "mark_10": NamedList("10",["mark_10"]),
+        # Italy
+        "centesimi_20": NamedList("20",["centesimi_20"]),
+        # Mexico
+        "peso_1": NamedList("1",["peso_1"]),
+        # United States
+        "dime": NamedList("Dimes",["barber_dime","mercury_dime","roosevelt_dime"]),
+        "quarter": NamedList("Quarters",["barber_quarter","standing_liberty_quarter","washington_quarter"]),
+        "half": NamedList("Halves",["walking_liberty_half","benjamin_half","kennedy_half_1","kennedy_half_2"]),
+        "dollar": NamedList("Dollars",["morgan_dollar","peace_dollar"])
     }
 
     denominations = {
+        # Canada
+        "canada_dollar": NamedList("Dollars",["canada_dollar_5"]),
+        # France
         "centimes": NamedList("Centimes", ["centimes_20", "centimes_50"]),
         "franc": NamedList(
             "Franc",
@@ -261,10 +486,24 @@ class Coins:
                 "franc_100",
             ],
         ),
+        # Germany
+        "mark": NamedList("Mark",["mark_10"]),
+        # Italy
+        "centesimi": NamedList("Centesimi",["centesimi_20"]),
+        # Mexico
+        "peso": NamedList("Peso",["peso_1"]),
+        # United States
+        "cents": NamedList("Cents",["dime","quarter","half"]),
+        "dollar": NamedList("Dollars",["dollar"]),
     }
 
     countries = {
+        "canada": NamedList("Canada",["canada_dollar"]),
         "france": NamedList("France", ["centimes", "franc"]),
+        "germany": NamedList("Germany", ["mark"]),
+        "italy": NamedList("Italy",["centesimi"]),
+        "mexico": NamedList("Mexico",["peso"]),
+        "united_states": NamedList("United States",["cents","dollar"])
     }
 
 
