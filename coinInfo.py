1f7f77eb6c8de9f34e77e01fdf859f8e8fa3a983
JMGillum <JMGillum@protonmail.com>
2025-07-24 18:24:13 -0700

    Added data for Germany

--- coinInfo.py
+++ coinInfo.py
@@ -532,6 +532,150 @@ class Coins:
             )
         ),
         # Germany
+        "pfennig_20": Node(
+            CoinData(
+                years=list(range(1873, 1920)),
+                weight=1.1111,
+                fineness=0.900,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=20,
+                denomination="Pfennig",
+            )
+        ),
+        "pfennig_50": Node(
+            CoinData(
+                years=list(range(1873, 1920)),
+                weight=2.7777,
+                fineness=0.900,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=50,
+                denomination="Pfennig",
+            )
+        ),
+        "mark_1_1": Node(
+            CoinData(
+                years=list(range(1873, 1920)),
+                weight=5.5555,
+                fineness=0.900,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=1,
+                denomination="Mark",
+            )
+        ),
+        "mark_1_2": Node(
+            CoinData(
+                years=list(range(1924,1934)),
+                weight=5.0000,
+                fineness=0.500,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=1,
+                denomination="Mark",
+            )
+        ),
+        "mark_2_1": Node(
+            CoinData(
+                years=list(range(1874, 1919)),
+                weight=11.1111,
+                fineness=0.900,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=2,
+                denomination="Mark",
+            )
+        ),
+        "mark_2_2": Node(
+            CoinData(
+                years=list(range(1924,1934)),
+                weight=10,
+                fineness=0.500,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=2,
+                denomination="Mark",
+            )
+        ),
+        "mark_2_3": Node(
+            CoinData(
+                years=list(range(1933,1940)),
+                weight=8.0000,
+                fineness=0.625,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=2,
+                denomination="Mark",
+            )
+        ),
+        "mark_3_1": Node(
+            CoinData(
+                years=list(range(1874, 1919)),
+                weight=16.6666,
+                fineness=0.900,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=3,
+                denomination="Mark",
+            )
+        ),
+        "mark_3_2": Node(
+            CoinData(
+                years=list(range(1924,1934)),
+                weight=15.0,
+                fineness=0.500,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=3,
+                denomination="Mark",
+            )
+        ),
+        "mark_5_1": Node(
+            CoinData(
+                years=list(range(1874, 1919)),
+                weight=27.7777,
+                fineness=0.900,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=5,
+                denomination="Mark",
+            )
+        ),
+        "mark_5_2": Node(
+            CoinData(
+                years=list(range(1871, 1916)),
+                weight=1.9913,
+                fineness=0.900,
+                retention=0.97,
+                metal=Metals.GOLD,
+                country="Germany",
+                face_value=5,
+                denomination="Mark",
+            )
+        ),
+        "mark_5_3": Node(
+            CoinData(
+                years=list(range(1924,1934)),
+                weight=25.0000,
+                fineness=0.500,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=5,
+                denomination="Mark",
+            )
+        ),
+        "mark_5_4": Node(
+            CoinData(
+                years=list(range(1933,1940)),
+                weight=13.8888,
+                fineness=0.900,
+                metal=Metals.SILVER,
+                country="Germany",
+                face_value=5,
+                denomination="Mark",
+            )
+        ),
         "mark_10": Node(
             CoinData(
                 years=list(range(1871, 1916)),
@@ -541,6 +685,18 @@ class Coins:
                 metal=Metals.GOLD,
                 country="Germany",
                 face_value=10,
+                denomination="Mark",
+            )
+        ),
+        "mark_20": Node(
+            CoinData(
+                years=list(range(1871, 1916)),
+                weight=7.965,
+                fineness=0.900,
+                retention=0.97,
+                metal=Metals.GOLD,
+                country="Germany",
+                face_value=20,
                 denomination="Mark",
             )
         ),
@@ -763,7 +919,21 @@ class Coins:
         "franc_100_1": ("franc_100","franc","france"),
         "franc_100_2": ("franc_100","franc","france"),
         "franc_100_3": ("franc_100","franc","france"),
+        "pfennig_20": ("pfennig_20","pfennig","germany"),
+        "pfennig_50": ("pfennig_50","pfennig","germany"),
+        "mark_1_1": ("mark_1","mark","germany"),
+        "mark_1_2": ("mark_1","mark","germany"),
+        "mark_2_1": ("mark_2","mark","germany"),
+        "mark_2_2": ("mark_2","mark","germany"),
+        "mark_2_3": ("mark_2","mark","germany"),
+        "mark_3_1": ("mark_3","mark","germany"),
+        "mark_3_2": ("mark_3","mark","germany"),
+        "mark_5_1": ("mark_5","mark","germany"),
+        "mark_5_2": ("mark_5","mark","germany"),
+        "mark_5_3": ("mark_5","mark","germany"),
+        "mark_5_4": ("mark_5","mark","germany"),
         "mark_10": ("mark_10","mark","germany"),
+        "mark_20": ("mark_20","mark","germany"),
         "centesimi_20": ("centesimi_20","centesimi","italy"),
         "peso_1": ("peso_1","peso","mexico"),
         "barber_dime": ("dime","cents","united_states"),
@@ -808,6 +978,18 @@ class Coins:
         "franc_10_1",
         "franc_20_2",
         "franc_100_1",
+        "pfennig_20",
+        "pfennig_50",
+        "mark_1_1",
+        "mark_1_2",
+        "mark_2_1",
+        "mark_2_2",
+        "mark_2_3",
+        "mark_3_1",
+        "mark_3_2",
+        "mark_5_1",
+        "mark_5_3",
+        "mark_5_4",
         "centesimi_20",
         "peso_1",
         "barber_dime",
@@ -835,7 +1017,9 @@ class Coins:
         "franc_50",
         "franc_100_2",
         "franc_100_3",
+        "mark_5_2",
         "mark_10",
+        "mark_20",
     ]
 
     # Updated in Coins.linkPurchases() to include keys to coins that have
@@ -865,7 +1049,14 @@ class Coins:
         "franc_50": NamedList("50", ["franc_50"]),
         "franc_100": NamedList("100", ["franc_100_1", "franc_100_2", "franc_100_3"]),
         # Germany
+        "pfennig_20": NamedList("20",["pfennig_20"]),
+        "pfennig_50": NamedList("50",["pfennig_50"]),
+        "mark_1": NamedList("1",["mark_1_1","mark_1_2"]),
+        "mark_2": NamedList("2",["mark_2_1","mark_2_2","mark_2_3"]),
+        "mark_3": NamedList("3",["mark_3_1","mark_3_2"]),
+        "mark_5": NamedList("5",["mark_5_1","mark_5_2","mark_5_3","mark_5_4"]),
         "mark_10": NamedList("10", ["mark_10"]),
+        "mark_20": NamedList("20", ["mark_20"]),
         # Italy
         "centesimi_20": NamedList("20", ["centesimi_20"]),
         # Mexico
@@ -910,7 +1101,8 @@ class Coins:
             ],
         ),
         # Germany
-        "mark": NamedList("Mark", ["mark_10"]),
+        "pfennig": NamedList("Pfennig",["pfennig_20","pfennig_50"]),
+        "mark": NamedList("Mark", ["mark_1","mark_2","mark_3","mark_5","mark_10","mark_20"]),
         # Italy
         "centesimi": NamedList("Centesimi", ["centesimi_20"]),
         # Mexico
@@ -923,7 +1115,7 @@ class Coins:
     countries = {
         "canada": NamedList("Canada", ["canada_cent","canada_sovereign","canada_dollar"]),
         "france": NamedList("France", ["centimes", "franc"]),
-        "germany": NamedList("Germany", ["mark"]),
+        "germany": NamedList("Germany", ["pfennig","mark"]),
         "italy": NamedList("Italy", ["centesimi"]),
         "mexico": NamedList("Mexico", ["peso"]),
         "united_states": NamedList("United States", ["cents", "dollar"]),
