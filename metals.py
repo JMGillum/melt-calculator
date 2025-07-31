9c5510a1910b5a5076d01d2b289ede5a4858438f
JMGillum <JMGillum@protonmail.com>
2025-07-31 14:12:30 -0700

    Added option for casting from string

--- metals.py
+++ metals.py
@@ -1,6 +1,6 @@
 """
    Author: Josh Gillum              .
-   Date: 18 July 2025              ":"         __ __
+   Date: 31 July 2025              ":"         __ __
                                   __|___       \ V /
                                 .'      '.      | |
                                 |  O       \____/  |
@@ -35,10 +35,24 @@ from enum import Enum
 class Metals(Enum):
     """Enumeration class for metal types"""
 
-    COPPER = 0
+    OTHER = 0
     SILVER = 1
     GOLD = 2
     PLATINUM = 3
     PALLADIUM = 4
     RHODIUM = 5
-    ELECTRUM = 6
+
+    def fromString(string):
+        s = string.lower()
+        if s == "silver" or s == "ag":
+            return Metals.SILVER
+        if s == "gold" or s == "au":
+            return Metals.GOLD
+        if s == "platinum" or s == "pt":
+            return Metals.PLATINUM
+        if s == "palladium" or s == "pd":
+            return Metals.PALLADIUM
+        if s == "rhodium" or s == "rh":
+            return Metals.RHODIUM
+        else:
+            return Metals.OTHER
