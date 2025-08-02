2a6b23d50d56e276e62aa482dcb1672caa174a7c
JMGillum <JMGillum@protonmail.com>
2025-08-01 21:36:41 -0700

    Renamed setup.sql and created bash script to merge the various setup scripts into a single script

---
+++ db/merge.sh
@@ -0,0 +1,6 @@
+> setup.sql
+cat setup_db.sql >> setup.sql
+cat setup_countries.sql >> setup.sql
+cat setup_denominations.sql >> setup.sql
+cat setup_values.sql >> setup.sql
+cat setup_coins.sql >> setup.sql
