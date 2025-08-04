a768086722803f7a3a81ecb955245d935ab55fdc
JMGillum <JMGillum@protonmail.com>
2025-08-04 14:18:58 -0700

    Refactored printColored()

--- colors.py
+++ colors.py
@@ -16,62 +16,46 @@ from config import show_color, colors_8_bit
 def printColored(text, fg_color="", custom_color=""):
     if not show_color:
         return text
-    fg_prefix = "\033["
+    prefix = "\033["
     suffix = "m"
     default = "\033[0m"
-
-    red = 31
-    pink = 31
-    blue = 34
-    teal = 36
-    green = 32
-    yellow = 33
-    bright_yellow = 33
-    purple = 35
-    magenta = 35
+    colors = {
+        "red": 31,
+        "pink": 31,
+        "blue": 34,
+        "teal": 36,
+        "green": 32,
+        "yellow": 33,
+        "bright_yellow": 33,
+        "purple": 35,
+        "magenta": 35,
+    }
 
     color_string = ""
-    colors = ""
+    ansi_string = ""
 
     if colors_8_bit:
-        fg_prefix = "\033[38:5:"
+        prefix = "\033[38:5:"
         default = "\033[39;49m"
-        red = 1
-        pink = 213
-        blue = 4
-        teal = 6
-        green = 2
-        yellow = 3
-        bright_yellow = 11
-        purple = 5
-        magenta = 163
+        colors.update(red=1)
+        colors.update(pink=213)
+        colors.update(blue=4)
+        colors.update(teal=6)
+        colors.update(green=2)
+        colors.update(yellow=3)
+        colors.update(bright_yellow=11)
+        colors.update(purple=5)
+        colors.update(magenta=163)
     if custom_color:
         color_string = custom_color
     else:
         test = fg_color.lower().strip()
-        match test:
-            case "red":
-                color_string = red
-            case "pink":
-                color_string = pink
-            case "blue":
-                color_string = blue
-            case "green":
-                color_string = green
-            case "yellow":
-                color_string = yellow
-            case "bright_yellow":
-                color_string = bright_yellow
-            case "purple":
-                color_string = purple
-            case "teal":
-                color_string = teal
-            case "magenta":
-                color_string = magenta
-            case _:
-                color_string = ""
-        colors += f"{fg_prefix}{color_string}{suffix}"
-    return f"{colors}{text}{default}"
+        try:
+            color_string = colors[test]
+        except KeyError:
+            color_string = ""
+        ansi_string = f"{prefix}{color_string}{suffix}"
+    return f"{ansi_string}{text}{default}"
 
 
 if __name__ == "__main__":
