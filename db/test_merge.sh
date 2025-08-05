3adc5d4cbe88b476044b9ca29b7b1554f9f5d88b
JMGillum <JMGillum@protonmail.com>
2025-08-04 18:04:49 -0700

    touch all files to make sure they exist first

--- db/test_merge.sh
+++ db/test_merge.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+touch test_setup_countries.sql
+touch test_setup_denominations.sql
+touch test_setup_values.sql
+touch tests_setup_coins.sql
 > test_setup.sql
 cat test_setup_db.sql >> test_setup.sql
 cat test_setup_countries.sql >> test_setup.sql
