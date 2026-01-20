5af6dbca6bc4c9f936f4011af52c4d5abe8c2003
JMGillum <JMGillum@protonmail.com>
2026-01-19 17:11:49 -0800

    Moved general purpose functions: getConfirmation(), getDate(), and selectEntry() to general.py

--- general.py
+++ general.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 def strToNum(num):
     fail = False
@@ -35,3 +36,79 @@ def strToNum(num):
             else:
                 fail = True
     return (fail,temp_num)
+
+
+def getConfirmation(prompt):
+    while True:
+        response = input(f"{prompt} (y/n): ").lower()
+        if response == 'y' or response == "yes":
+            return True
+        elif response == 'n' or response == "no":
+            return False
+        else:
+            print("You must enter either 'y' or 'n'.")
+
+
+def getDate():
+    date_prompt = "Enter date as either: 'D.M.Y', 'M/D/Y', or 'Y-M-D': "
+    tries = 0
+    found_date = None
+    response = None
+    while tries == 0 or response or response is None:
+        if tries == 0 or response is None:
+            response = input(date_prompt)
+        else:
+            response = input(f'Press enter to accept {found_date.strftime("%d %B %Y")} or {date_prompt}')
+        if tries <= 0 or response:
+            tries += 1
+            if response:
+                values_dot = response.split(".")
+                values_slash = response.split("/")
+                values_dash = response.split("-")
+                day = -1
+                month = -1
+                year = -1
+
+                if len(values_dot) == 3:
+                    day,month,year = values_dot
+                elif len(values_slash) == 3:
+                    month,day,year = values_slash
+                elif len(values_dash) == 3:
+                    year,month,day = values_dash
+
+                try:
+                    year = int(year)
+                    if year < 100:
+                        year += 2000
+                    found_date = datetime(year,int(month),int(day))
+                except TypeError:
+                    print("All values must be numeric.")
+                    response = None
+                except ValueError as e:
+                    print(f"Values are outside of the acceptable range. {e}")
+                    response = None
+                # Ensures date is within the range that can be sent to the database
+    return found_date.strftime("%Y-%m-%d")
+
+
+def selectEntry(entries):
+    for i in range(len(entries)):
+        if len(entries) > 1:
+            print(f"{i+1}: {entries[i]}")
+        else:
+            print(f"{entries[i]}")
+    entry_id = 0
+    if len(entries) > 1: # Multiple results from search
+        while True:
+            try:
+                entry_id = int(input("Enter number for entry to select it: "))
+            except ValueError:
+                print("Must be numeric.")
+                continue
+            if entry_id <= 0 or entry_id > len(entries):
+                print("Value out of range")
+                continue
+            else:
+                entry_id -= 1
+                break
+    return entry_id
--- managePurchases.py
+++ managePurchases.py
@@ -1,61 +1,25 @@
-from db_interface import DB_Interface
+#   Author: Josh Gillum              .
+#   Date: 19 January 2026           ":"         __ __
+#                                  __|___       \ V /
+#                                .'      '.      | |
+#                                |  O       \____/  |
+#~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~
+#
+#    This script provides a function for managing the purchases stored within 
+#    the database. It is still WIP and currently can add or remove purchases.
+#
+#    Future goal is to be able to modify existing purchases to change quantities
+#    or prices (to fix an error or to "sell off" part of the purchase)
+#
+#~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~
+
 import config
-from datetime import datetime
 from coins import Coins
-from coinData import CoinData,Purchase
+from coinData import CoinData, Purchase
 import data
-import argparse
 
-def getConfirmation(prompt):
-    while True:
-        response = input(f"{prompt} (y/n): ").lower()
-        if response == 'y' or response == "yes":
-            return True
-        elif response == 'n' or response == "no":
-            return False
-        else:
-            print("You must enter either 'y' or 'n'.")
+from general import getConfirmation, getDate, selectEntry
 
-def getDate():
-    date_prompt = "Enter date as either: 'D.M.Y', 'M/D/Y', or 'Y-M-D': "
-    tries = 0
-    found_date = None
-    response = None
-    while tries == 0 or response or response is None:
-        if tries == 0 or response is None:
-            response = input(date_prompt)
-        else:
-            response = input(f'Press enter to accept {found_date.strftime("%d %B %Y")} or {date_prompt}')
-        if tries <= 0 or response:
-            tries += 1
-            if response:
-                values_dot = response.split(".")
-                values_slash = response.split("/")
-                values_dash = response.split("-")
-                day = -1
-                month = -1
-                year = -1
-
-                if len(values_dot) == 3:
-                    day,month,year = values_dot
-                elif len(values_slash) == 3:
-                    month,day,year = values_slash
-                elif len(values_dash) == 3:
-                    year,month,day = values_dash
-
-                try:
-                    year = int(year)
-                    if year < 100:
-                        year += 2000
-                    found_date = datetime(year,int(month),int(day))
-                except TypeError:
-                    print("All values must be numeric.")
-                    response = None
-                except ValueError as e:
-                    print(f"Values are outside of the acceptable range. {e}")
-                    response = None
-                # Ensures date is within the range that can be sent to the database
-    return found_date.strftime("%Y-%m-%d")
 
 def getPurchaseInformation():
     # Gets purchase date,quantity, and price from user
@@ -98,6 +62,7 @@ def getPurchaseInformation():
     print(f"({date_string}) {config.currency_symbol}{purchase['unit_price']} x {purchase['quantity']}")
     return purchase
 
+
 def getSpecificCoinInformation():
     # Gets specific coin information from user
     print("-------------------------------Specific Coin-------------------------------------")
@@ -127,6 +92,7 @@ def getSpecificCoinInformation():
             specific_coin["year"] = specific_coin["mintmark"] = None
     return specific_coin
 
+
 def pushSpecificCoin(db,specific_coin,coin):
     specific_coin_id = None
     
@@ -155,29 +121,6 @@ def pushPurchase(db,coin,purchase,specific_coin_id):
         exit(1)
     else: # Successful
         print("Added purchase successfully.")
-
-
-def selectEntry(entries):
-    for i in range(len(entries)):
-        if len(entries) > 1:
-            print(f"{i+1}: {entries[i]}")
-        else:
-            print(f"{entries[i]}")
-    entry_id = 0
-    if len(entries) > 1: # Multiple results from search
-        while True:
-            try:
-                entry_id = int(input("Enter number for entry to select it: "))
-            except ValueError:
-                print("Must be numeric.")
-                continue
-            if entry_id <= 0 or entry_id > len(entries):
-                print("Value out of range")
-                continue
-            else:
-                entry_id -= 1
-                break
-    return entry_id
 
 
 def getCoinInformation(db,additional_search_args:dict=None):
