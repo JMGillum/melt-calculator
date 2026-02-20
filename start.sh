52ae39c3152ae100d75a26710603683bf0294bf0
Josh Gillum <80538855+JMGillum@users.noreply.github.com>
2026-02-19 19:37:38 -0800

    Update start.sh to include venv code

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Uncomment two lines below to run program within anaconda environment
 #eval "$(conda shell.bash hook)"
-#conda activate myenv
-./main.py "$@"
+#conda activate metals
+
+# Uncomment line below to run program within venv environment
+# source /absolute/path/to/project/venv/bin/activate
+absolute/path/to/project/main.py "$@"
 
